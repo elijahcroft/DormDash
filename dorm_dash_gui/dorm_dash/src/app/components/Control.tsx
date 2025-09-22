c7814ebc43bb7eba6dd89afb1ef1c6417df86b32
elijah <elijahcroft49@gmail.com>
2025-09-21 22:42:06 -0700

    control panel comp

--- dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
+++ dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
@@ -3,7 +3,7 @@ export default function Control(){
     return (
         <div className="flex flex-row rounded-4xl bg-[#F1F0E4]  min-w-128 min-h-200 outline">
             <div className="min-w-full max-h-20 bg-[#37353E] text-center rounded-t-4xl">
-                <p>Control panel</p>
+                <p className="p-5 text-3xl">Control panel:</p>
             </div>
             
         </div>
