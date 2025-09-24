421d8d89ff71adfa56177fb33178174ed10944b6
elijah <elijahcroft49@gmail.com>
2025-09-24 12:31:01 -0700

    working on gui

--- dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
+++ dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
@@ -1,11 +1,15 @@
 
 export default function Control(){
     return (
-        <div className="flex flex-row rounded-4xl bg-[#F1F0E4]  min-w-128 min-h-200 outline">
-            <div className="min-w-full max-h-20 bg-[#37353E] text-center rounded-t-4xl">
+        <div className="flex flex-col rounded-4xl bg-[#F1F0E4]  min-w-140 min-h-200 outline">
+            <div className="min-w-full min-h-30 bg-[#37353E] text-center rounded-t-4xl">
                 <p className="p-5 text-3xl">Control panel:</p>
             </div>
-            
+            <div className="flex flex-row items-stretch bg-[#DDDAD0] min-h-10 text-[#44444E] ">
+                <div className="flex min-w-1/3 text-center">hi</div>
+                <div className="flex min-w-1/3 ">tab2</div>
+                <div className="flex min-w-1/3">tab3</div>
+            </div>
         </div>
     )
 }
