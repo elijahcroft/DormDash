9ef3495e9a16de2af6a6f57f27553b83475c375c
JoshDuenas <joshd@csu.fullerton.edu>
2025-09-25 16:29:09 -0700

    Changed the name of the contents for Control Panel into 'Navigating', 'Add item', and 'Delivering'. Created a Tab.tsx file

--- dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
+++ dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
@@ -1,14 +1,14 @@
 
 export default function Control(){
     return (
-        <div className="flex flex-col rounded-4xl bg-[#F1F0E4]  min-w-140 min-h-200 outline">
+        <div className="flex flex-col rounded-4xl bg-[#F1F0E4]  min-w-140 min-h-120 outline">
             <div className="min-w-full min-h-30 bg-[#37353E] text-center rounded-t-4xl">
                 <p className="p-5 text-3xl">Control panel:</p>
             </div>
             <div className="flex flex-row items-stretch bg-[#DDDAD0] min-h-10 text-[#44444E] ">
-                <div className="flex min-w-1/3 justify-center">tab 1</div>
-                <div className="flex min-w-1/3b  justify-center">tab2</div>
-                <div className="flex min-w-1/3 justify-center   ">tab3</div>
+                <div className="flex min-w-1/3 justify-center">Navigating</div>
+                <div className="flex min-w-1/3b  justify-center">Add item</div>
+                <div className="flex min-w-1/3 justify-center   ">Delivering</div>
             </div>
         </div>
     )
