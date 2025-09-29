dc6ed866a5a4baec17e0d9209ea324d85557f4a7
JoshDuenas <joshd@csu.fullerton.edu>
2025-09-29 14:46:12 -0700

    Aligned the 'Navigating', 'Add item', and 'Delivering' correctly. No functionality added yet

--- dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
+++ dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
@@ -3,12 +3,12 @@ export default function Control(){
     return (
         <div className="flex flex-col rounded-4xl bg-[#F1F0E4]  min-w-140 min-h-120 outline">
             <div className="min-w-full min-h-30 bg-[#37353E] text-center rounded-t-4xl">
-                <p className="p-5 text-3xl">Control panel:</p>
+                <p className="p-5 text-3xl text-white">Control Panel:</p>
             </div>
             <div className="flex flex-row items-stretch bg-[#DDDAD0] min-h-10 text-[#44444E] ">
-                <div className="flex min-w-1/3 justify-center">Navigating</div>
-                <div className="flex min-w-1/3b  justify-center">Add item</div>
-                <div className="flex min-w-1/3 justify-center   ">Delivering</div>
+                <div className="flex flex-1 justify-center items-center">Navigating</div>
+                <div className="flex flex-1  justify-center items-center">Add item</div>
+                <div className="flex flex-1 justify-center items-center">Delivering</div>
             </div>
         </div>
     )
