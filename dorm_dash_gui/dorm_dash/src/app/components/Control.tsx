af962ec1e8e6e8d82e7c24a09e120fe11b75c62b
JoshDuenas <joshd@csu.fullerton.edu>
2025-09-30 10:37:35 -0700

    Aligned the 'Control Panel:' to look similar to the GUI mockup

--- dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
+++ dorm_dash_gui/dorm_dash/src/app/components/Control.tsx
@@ -2,7 +2,7 @@
 export default function Control(){
     return (
         <div className="flex flex-col rounded-4xl bg-[#F1F0E4]  min-w-140 min-h-120 outline">
-            <div className="min-w-full min-h-30 bg-[#37353E] text-center rounded-t-4xl">
+            <div className="min-w-full min-h-30 bg-[#37353E] text-left py-4 px-6 rounded-t-4xl">
                 <p className="p-5 text-3xl text-white">Control Panel:</p>
             </div>
             <div className="flex flex-row items-stretch bg-[#DDDAD0] min-h-10 text-[#44444E] ">
